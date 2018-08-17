a1c56e5ff48e05766a8e6ae1fd07256d25f4e4ff
Andrew Odendaal <andrew@hsmoore.com>
2018-08-17 10:28:05 +0100

    Update to v1.1

--- popup.js
+++ popup.js
@@ -3,14 +3,6 @@ var data;
 var host;
 var key;
 
-chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true
-}, function(tabs) {
-    var tab = tabs[0];
-    host = extractHostname(tab.url);
-    key = 'runjavascript_'+host;
-});
 
 function extractHostname(url) {
     var hostname;
@@ -41,15 +33,54 @@ function saveChanges() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  editor = ace.edit("editor");
-  document.getElementById('runJavascript').addEventListener('click', saveChanges);
+function update_ace_placeholder() {
+    var shouldShow = !editor.session.getValue().length;
+    var node = editor.renderer.emptyMessageNode;
 
-  chrome.storage.sync.get(function(obj) {
-      var js = obj['runjavascript_'+host];
-      if (js == undefined || js == 'undefined') {
-          js = '';
-      }
-      editor.setValue(js);
-  });
+    var node2 = editor.renderer.emptyMessageNode2;
+    if (!shouldShow && node) {
+        editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode);
+        editor.renderer.emptyMessageNode = null;
+
+        editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode2);
+        editor.renderer.emptyMessageNode2 = null;
+    } else if (shouldShow && !node) {
+        node = editor.renderer.emptyMessageNode = document.createElement("div");
+        node.textContent = "Add your Javascript here and click 'Save and Run'"
+        node.className = "ace_invisible ace_emptyMessage"
+        node.style.padding = "200px 5px 5px 5px"
+        editor.renderer.scroller.appendChild(node);
+
+        node2 = editor.renderer.emptyMessageNode2 = document.createElement("div");
+        node2.textContent = "It will be run now and everytime you visit this domain again in the future!"
+        node2.className = "ace_invisible ace_emptyMessage"
+        node2.style.padding = "10px 5px 5px 5px"
+        editor.renderer.scroller.appendChild(node2);
+    }
+}
+
+chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+}, function(tabs) {
+    var tab = tabs[0];
+    host = extractHostname(tab.url);
+    key = 'runjavascript_'+host;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    editor = ace.edit("editor");
+    document.getElementById('runJavascript').addEventListener('click', saveChanges);
+
+    chrome.storage.sync.get(function(obj) {
+        var js = obj['runjavascript_'+host];
+        if (js == undefined || js == 'undefined') {
+            js = '';
+        }
+        editor.setValue(js);
+    });
+
+    editor.on("input", update_ace_placeholder);
+    setTimeout(update_ace_placeholder, 100);
+
 });
